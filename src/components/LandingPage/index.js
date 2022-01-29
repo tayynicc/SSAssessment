412a93f6b6fe6175d162fd081b15081b1977cc15
tayynicc <digitalidentity21@gmail.com>
2022-01-29 16:29:58 -0700

    inital populate

---
+++ src/components/LandingPage/index.js
@@ -0,0 +1,20 @@
+import  './LandingPage.css'
+
+import GatherUsrData from './Forms/GatherUsrData'
+
+
+function LandingPage (){
+
+
+  return (
+    <>
+      <div className='container__left'>
+
+      </div>
+    </>
+
+  )
+};
+
+export default LandingPage;
+
