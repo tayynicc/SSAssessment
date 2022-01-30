b2c5c71ff4833a1956e85408b3dd4a6a2ce6fee2
tayynicc <digitalidentity21@gmail.com>
2022-01-29 17:18:19 -0700

    session slice of state

--- src/store/session.js
+++ src/store/session.js
@@ -50,11 +50,65 @@ export const login = (email, password) => async (dispatch) => {
       return data.errors;
     }
   } else {
-    return ['Login Error encountered. Try again.']
+    return ['Login Error encountered. Please Try again.']
   }
 }
 
 
 export const logout = () => async(dispatch) => {
-  
+  const res = await fetch('', {
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  });
+
+  if(res.ok){
+    dispatch(removeUser())
+  }
+}
+
+export const signUp = (fName, lName, email, password, isActive, address, city, state, zip) => async (dispatch) => {
+  const res = await fetch('', {
+    method: 'POST',
+    headers:{
+      'Content-Type':'application/json',
+    },
+    body: JSON.stringify({
+      fName,
+      lName,
+      email,
+      password,
+      isActive,
+      address,
+      city, 
+      state, 
+      zip,
+    }),
+  });
+
+
+  if(res.ok){
+    const data = await res.json();
+    dispatch(setUser(data))
+    return null;
+  } else if (res.status < 500 ){
+    const data = await res.json();
+    if (data.errors){
+      return data.errors;
+    }
+  } else {
+    return ['Signup error occured. Please try again']
+  }
+}
+
+
+export default function reducer(state = initialState, action ){
+  switch(action.type){
+    case SET_USER:
+      return { user: action.payload }
+    case REMOVE_USER:
+      return { user: null }
+    default: 
+      return state; 
+  }
 }
